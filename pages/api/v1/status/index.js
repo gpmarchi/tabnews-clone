daf54804bfdec9c94a53f05449ff48dcf4d0eea6
gpmarchi <gustavomarchi@gmail.com>
2024-06-20 11:04:09 -0300

    style: lint file and remove white spaces

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,7 @@ async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
 
   console.log(result.rows);
-  
+
   response.status(200).json({
     chave: "alunos do curso.dev são pessoas acima da média",
   });
