49af55a75a84e0eb3dbfc4d9bfb5bef32d8dfbdd
gpmarchi <gustavomarchi@gmail.com>
2025-09-18 18:20:11 -0300

    test: adjust for new status codes and guarantee migrations won't be executed twice

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -8,9 +8,10 @@ beforeAll(cleanDatabase)
 
 test('GET to /api/v1/migrations should return 200', async () => {
   const response = await fetch('http://localhost:3000/api/v1/migrations')
+  expect(response.status).toBe(200)
+
   const responseBody = await response.json()
 
-  expect(response.status).toBe(200)
   expect(Array.isArray(responseBody)).toBe(true)
   expect(responseBody.length).toBeGreaterThan(0)
 })
