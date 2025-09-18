44da98686d7066bd131e4a1b2b734b0083b6932c
gpmarchi <gustavomarchi@gmail.com>
2025-09-18 17:47:58 -0300

    test: clean database before tests execution

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,9 +1,17 @@
-test("POST to /api/v1/migrations should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
-  const responseBody = await response.json();
+import database from 'infra/database'
 
-  expect(response.status).toBe(200);
-  expect(Array.isArray(responseBody)).toBe(true);
-});
+async function cleanDatabase() {
+  await database.query('drop schema public cascade; create schema public;')
+}
+
+beforeAll(cleanDatabase)
+
+test('POST to /api/v1/migrations should return 200', async () => {
+  const response = await fetch('http://localhost:3000/api/v1/migrations', {
+    method: 'POST',
+  })
+  const responseBody = await response.json()
+
+  expect(response.status).toBe(200)
+  expect(Array.isArray(responseBody)).toBe(true)
+})
