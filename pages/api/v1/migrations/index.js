f673bc53b9b2723dc1ea7774130334c00d726cab
gpmarchi <gustavomarchi@gmail.com>
2025-09-18 17:46:12 -0300

    style: add prettier config rules and lint fix all files

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,32 +1,32 @@
-import migrationRunner from "node-pg-migrate";
-import { join } from "node:path";
+import migrationRunner from 'node-pg-migrate'
+import { join } from 'node:path'
 
 export default async function migrations(request, response) {
-  if (request.method === "GET") {
+  if (request.method === 'GET') {
     const migrations = await migrationRunner({
       databaseUrl: process.env.DATABASE_URL,
       dryRun: true,
-      dir: join("infra", "migrations"),
-      direction: "up",
+      dir: join('infra', 'migrations'),
+      direction: 'up',
       verbose: false,
-      migrationsTable: "pgmigrations",
-    });
+      migrationsTable: 'pgmigrations',
+    })
 
-    return response.status(200).json(migrations);
+    return response.status(200).json(migrations)
   }
 
-  if (request.method === "POST") {
+  if (request.method === 'POST') {
     const migrations = await migrationRunner({
       databaseUrl: process.env.DATABASE_URL,
       dryRun: false,
-      dir: join("infra", "migrations"),
-      direction: "up",
+      dir: join('infra', 'migrations'),
+      direction: 'up',
       verbose: true,
-      migrationsTable: "pgmigrations",
-    });
+      migrationsTable: 'pgmigrations',
+    })
 
-    return response.status(200).json(migrations);
+    return response.status(200).json(migrations)
   }
 
-  return response.status(405).end();
+  return response.status(405).end()
 }
